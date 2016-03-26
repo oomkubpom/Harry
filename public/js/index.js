6f16e379d54f0dd93514a683c968340cb6294efc
oomkubpom <oomkubpom@gmail.com>
2016-03-26 17:53:49 +0700

    edit

--- public/js/index.js
+++ public/js/index.js
@@ -2,65 +2,65 @@
 var eppingList = angular.module('root', [])
 eppingList.controller('index', ['$scope', function ($scope) {
   $scope.list = []
-  $scope.amount = []
+  $scope.num = []
   $scope.discountBook = 0
   $scope.sell = 0
-  $scope.getTotal = function () {
-    var total = 0
+  $scope.gett = function () {
+    var t = 0
     for (var i = 0; i < $scope.list.length; i++) {
-      total += ($scope.list[i].amount * 100)
+      t += ($scope.list[i].num * 100)
     }
-    return total
+    return t
   }
 
   $scope.remove = function (item) {
     var index = $scope.list.indexOf(item)
     $scope.list.splice(index, 1)
-    var amountproduct = []
+    var numproduct = []
     for (var i = 0; i < $scope.list.length; i++) {
-      if (typeof $scope.list[i].amount !== 'undefined') {
-        amountproduct.push($scope.list[i].amount)
+      if (typeof $scope.list[i].num !== 'undefined') {
+        numproduct.push($scope.list[i].num)
       }
-      $scope.amount = amountproduct
+      $scope.num = numproduct
     }
-    $scope.getDiscount($scope.amount)
+    $scope.getDiscount($scope.num)
   }
 
-  $scope.clearAll = function (list) {
+  $scope.clear = function (list) {
     var length = list.length
     list.splice(0, length)
   }
-  $scope.addItem = function (ep, name) {
+  $scope.add = function (ep, name) {
     if (check($scope.list, ep)) {
       var index = findArrBook($scope.list, ep)
-      $scope.list[index].amount += 1
+      $scope.list[index].num += 1
     } else {
       var data = {
         ep: ep,
         name: name,
-        amount: 1
+        num: 1
       }
       $scope.list.push(data)
     }
 
     $scope.list.sort(function (a, b) {
-      if (a.amount > b.amount) {
+      if (a.num > b.num) {
         return -1
       }
-      if (a.amount < b.amount) {
+      if (a.num < b.num) {
         return 1
       }
       return 0
     })
 
-    var amountproduct = []
+    var numproduct = []
     for (var i = 0; i < $scope.list.length; i++) {
-      if (typeof $scope.list[i].amount !== 'undefined') {
-        amountproduct.push($scope.list[i].amount)
+      if (typeof $scope.list[i].num !== 'undefined') {
+        numproduct.push($scope.list[i].num)
       }
-      $scope.amount = amountproduct
+      $scope.num = numproduct
     }
-    $scope.getDiscount($scope.amount)
+    $scope.getDiscount($scope.num)
   }
   var check = function (list, ep) {
     for (var i = 0; i < list.length; i++) {
@@ -76,17 +76,17 @@ eppingList.controller('index', ['$scope', function ($scope) {
       }
     }
   }
-  $scope.getDiscount = function (amount) {
+  $scope.getDiscount = function (num) {
     $scope.sell = 0
     var countlist = 0
     var exit = 0
-    console.log(amount)
+    console.log(num)
     do {
-      for (var i = 0; i < amount.length; i++) {
-        if (amount[i] !== 0) {
+      for (var i = 0; i < num.length; i++) {
+        if (num[i] !== 0) {
           countlist += 1
         }
-        if (amount[i] === 0) {
+        if (num[i] === 0) {
           countlist += 0
           break
         }
@@ -115,10 +115,10 @@ eppingList.controller('index', ['$scope', function ($scope) {
         exit = 1
         console.log('exit')
       }
-      for (var o = 0; o < amount.length; o++) {
-        if (amount[o] > 0) {
-          amount[o] -= 1
-          countlist = 0 
+      for (var o = 0; o < num.length; o++) {
+        if (num[o] > 0) {
+          num[o] -= 1
+          countlist = 0
         }
       }
     } while (exit !== 1)
